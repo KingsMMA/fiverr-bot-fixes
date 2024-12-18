8ee104fea53fc214446d16aa3616eb0bc6787b5e
KingsDev <63374258+KingsMMA@users.noreply.github.com>
2024-12-18 23:46:02 +1000

    Reverted time to 8 NY time

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -25,7 +25,7 @@ const event: BotEvent = {
         client.cache.set("shopMessageID", shopMessageID);
 
         cron.schedule(
-            "0 13 * * *",
+            "0 8 * * *",
             async () => {
                 const settings = await prisma.setting.findFirst();
                 if (settings!.resetCheckins) await resetCheckins(prisma, guild);
